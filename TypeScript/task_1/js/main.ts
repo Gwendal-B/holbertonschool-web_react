2bb3721f758cb4d316bbf06fb16f7092f48be1b0
Gwendal-B <12309@holbertonstudents.com>
2026-05-19 17:53:42 +0200

    added task 4 - 5

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,6 +11,37 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(
+    private firstName: string,
+    private lastName: string
+  ) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const StudentClassConstructor: StudentConstructor = StudentClass;
+const student = new StudentClassConstructor('John', 'Doe');
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -18,8 +49,6 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
-
-console.log(teacher3);
 
 const director1: Directors = {
   firstName: 'John',
@@ -29,15 +58,13 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
-console.log(director1);
-
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
 const printTeacher: printTeacherFunction = (
   firstName: string,
   lastName: string
 ): string => `${firstName.charAt(0)}. ${lastName}`;
 
+console.log(director1);
+console.log(teacher3);
 console.log(printTeacher('John', 'Doe'));
+console.log(student.displayName());
+console.log(student.workOnHomework());
